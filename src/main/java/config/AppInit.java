d27247a2f05c9a9b3df0e05910e4eb46f9382f6e
archContaminatr <mahov45na@mail.ru>
2023-04-23 17:02:07 +0600

    misc added

--- src/main/java/config/AppInit.java
+++ src/main/java/config/AppInit.java
@@ -1,2 +1,17 @@
-package config;public class AppInit {
+package config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {JavaConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
 }
